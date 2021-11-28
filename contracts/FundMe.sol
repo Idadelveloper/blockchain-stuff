e3d3b7218839814d8e8e053fc4b1c2716d980d6c
Ida Delphine <56026636+Idadelveloper@users.noreply.github.com>
2021-11-28 18:26:30 +0100

    A smart contract that can accept payment

---
+++ contracts/FundMe.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+
+// Simple crowdsourcing application where users can fund and an admin withdraw those funds
+
+pragma solidity >=0.6.6 <0.9.0;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+contract FundMe {
+
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+    address public owner;
+    
+    function fund() public payable {
+        uint256 minimunUSD = 50 * (10 ** 18);
+        require(getConversionRate(msg.value) >= minimunUSD, "You need to spend more ETH");
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+    }
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function getVersion() public view returns (uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        return priceFeed.version();
+    }
+
+    function getPrice() public view returns(uint256){
+        // Gets the price of Ethereum in USD
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        (,int256 answer,,,) = priceFeed.latestRoundData();
+        return uint256(answer * 10000000000);
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns(uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
+        return ethAmountInUsd;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function withdraw() payable onlyOwner public{
+        // only owner or admin can withdraw funds
+        msg.sender.transfer(address(this).balance);
+        for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+    }
+}
+
